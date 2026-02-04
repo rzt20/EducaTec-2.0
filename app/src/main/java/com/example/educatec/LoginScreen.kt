af01e50836251a93bd1b7ff741c63ee1aedc48a4
rzt20 <226w0702@zongolica.tecnm.mx>
2026-02-04 13:27:35 -0600

    Se agrego el formulario de iniciar sesion en LoginScreen.kt

--- app/src/main/java/com/example/educatec/LoginScreen.kt
+++ app/src/main/java/com/example/educatec/LoginScreen.kt
@@ -1,20 +1,41 @@
 package com.example.educatec
 
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.Button
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
+import androidx.compose.foundation.layout.*
+import androidx.compose.material3.*
+import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
 import androidx.navigation.NavHostController
 
 @Composable
 fun LoginScreen(navController: NavHostController) {
-    Box(
+
+    var correo by remember { mutableStateOf("") }
+    var password by remember { mutableStateOf("") }
+
+    Column(
         modifier = Modifier.fillMaxSize(),
-        contentAlignment = Alignment.Center
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
     ) {
+
+        TextField(
+            value = correo,
+            onValueChange = { correo = it },
+            label = { Text("Correo electrónico") }
+        )
+
+        Spacer(modifier = Modifier.height(16.dp))
+
+        TextField(
+            value = password,
+            onValueChange = { password = it },
+            label = { Text("Contraseña") }
+        )
+
+        Spacer(modifier = Modifier.height(24.dp))
+
         Button(onClick = {
             navController.navigate("home")
         }) {
